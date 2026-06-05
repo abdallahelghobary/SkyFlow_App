7a22dcc5006d1795a089dab1446a16b1a586dc0d
abdallahelghobary <abdualahghobary@gmail.com>
2026-06-05 12:20:33 +0300

    Change Background Color

--- lib/widget/weather_info_body.dart
+++ lib/widget/weather_info_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit.dart';
+import 'package:weather_app/main.dart';
 import 'package:weather_app/models/weather_model.dart';
 
 class WeatherInfoBody extends StatelessWidget {
@@ -10,51 +11,72 @@ class WeatherInfoBody extends StatelessWidget {
     WeatherModel weatherModel = BlocProvider.of<GetWeatherCubit>(
       context,
     ).weatherModel!;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
 
-      children: [
-        Center(child: CustamText()),
-        Center(
-          child: Text(
-            'Updated at ${weatherModel.date.hour}:${weatherModel.date.minute.toString().padLeft(2, '0')}',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
+    return Container(
 
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+decoration: BoxDecoration(
+  gradient: LinearGradient(
+    colors: [
+    getThemeColor(weatherModel.weatherCondition),
+    getThemeColor(weatherModel.weatherCondition)[300]!,
+    getThemeColor(weatherModel.weatherCondition)[50]!,
+
+
+    ],
+    begin: AlignmentGeometry.topCenter,
+  end: AlignmentGeometry.bottomCenter)
+),      
+      child: Padding(
+      
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        
           children: [
-            // Text(weatherModel.image, style: TextStyle(fontSize: 100)),
-            Image.network('https:${weatherModel.image}'),
-
-            Text(
-              weatherModel.maxTemp.toString(),
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            Center(child: CustamText()),
+            Center(
+              child: Text(
+                'Updated at ${weatherModel.date.hour}:${weatherModel.date.minute.toString().padLeft(2, '0')}',
+                style: TextStyle(fontSize: 20),
+              ),
             ),
-
-            Column(
+        const SizedBox(height: 30,),
+        
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
+                // Text(weatherModel.image, style: TextStyle(fontSize: 100)),
+                Image.network('https:${weatherModel.image}'),
+                
                 Text(
-                  'Maxtemp: ${weatherModel.maxTemp.round()}',
-                  style: TextStyle(fontSize: 20),
+                  weatherModel.temperature.toString(),
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
                 ),
-                Text(
-                  'Maxtemp: ${weatherModel.minTemp.round()}',
-                  style: TextStyle(fontSize: 20),
+        
+                Column(
+                  children: [
+                    Text(
+                      'Maxtemp: ${weatherModel.maxTemp.round()}',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      'Maxtemp: ${weatherModel.minTemp.round()}',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
                 ),
               ],
             ),
+        const SizedBox(height: 15,),
+            Center(
+              child: Text(
+                weatherModel.weatherCondition,
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              ),
+            ),
           ],
         ),
-
-        Center(
-          child: Text(
-            weatherModel.weatherCondition,
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
@@ -67,7 +89,7 @@ class CustamText extends StatelessWidget {
     final weatherModel = BlocProvider.of<GetWeatherCubit>(context).weatherModel;
     return Text(
       weatherModel?.cityName ?? '',
-      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
     );
   }
 }
